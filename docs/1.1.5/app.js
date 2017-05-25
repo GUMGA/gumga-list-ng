35a4c8094712e457446ad9de71a749ace5ef8fd8
Mateus Miranda <info.mateusmiranda@gmail.com>
2017-05-25 16:37:23 -0300

    exemplo de documentação

---
+++ docs/1.1.5/app.js
@@ -0,0 +1,19 @@
+angular.module('app', ['gumga.list'])
+  .controller('Ctrl', function($http){
+    var ctrl = this;
+
+    $http.get('https://api.github.com/repos/gumga/gumga-list-ng/releases')
+      .then(function(resp){
+        ctrl.exemplo = resp.data;
+      })
+
+    ctrl.tableConfig = {
+      columns: 'tag_name, published_at',
+      checkbox: true,
+      materialTheme:true,
+      columnsConfig: [
+
+      ]
+    }
+
+  })
